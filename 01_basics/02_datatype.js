7515e68316795cb2c9822bd099a4e5700bce2987
Mahi darji <mahidarji03@gmail.com>
2026-01-19 16:37:01 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+//alert(3 + 3)
+
+
+console.log(3 + 3)
+
+console.log("Mahi")
+
+let name = "Mahi"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// data types
+    // number => 2 to power 53
+    // bigint => big number
+    // string => " "
+    // boolean => true / false
+    // null => standalone value/empty value
+    // undefined => value not define /not assign value
+    // symbol => unique
+
+    // object
+
+console.log(typeof undefined);// undefined
+console.log(typeof null);// object
