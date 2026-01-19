cf265488cc8f444bbd286645ca2c7c58a86f4cfe
Mahi darji <mahidarji03@gmail.com>
2026-01-19 16:37:10 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1223345
+let accountEmail = "mahi@gmail.com"
+var accountPassword = "12345"
+accountCity = "ahemdabad"
+
+
+// accountId = 2 not allowed
+
+accountEmail = "yash@gmail.com"
+accountPassword = 121212
+accountCity = "jaipur"
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
