052c268a4249f01fac032e22ad6fb0bc47b3e3c6
Mahi darji <mahidarji03@gmail.com>
2026-01-29 15:08:19 +0000

    if statment

---
+++ 04_control_flow/one.js
@@ -0,0 +1,48 @@
+//if
+// const isUserLoggedIn = true
+// const temprature = 41
+
+// if (temprature === 41) {
+//     console.log("less than 50");   
+// }else{
+//     console.log("temprature is greater than 50");     
+// }
+
+
+// < , > , <= , >= , == , != ,===(equal also type define) , !==
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User power ${power}`);
+// }
+// console.log(`User power ${power}`);
+
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test");
+
+// if (balance < 500) {
+//     console.log("less than 500");   
+// }else if (balance < 750) {
+//     console.log("less than 750");
+// }else if (balance < 900) {
+//     console.log("less than 750");
+// }else{
+//     console.log("less than 1200");
+// }
+
+const UserLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = true
+const loggedInFromEmail = true
+
+if (UserLoggedIn && debitCard && 2 ==   2) {
+    console.log("allow to buy course");   
+}
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged In");  
+}
